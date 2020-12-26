bead12018b6f29f5722eac33671555ff984a90e8
Yitzchak Horowitz <yitzchakhorowitz321@gmail.com>
2020-12-26 21:02:25 +0200

    Solution name change

--- TRMData/dbo/Tables/Inventory.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Inventory]
-(
-	[Id] INT NOT NULL PRIMARY KEY IDENTITY,
-	[ProductId] INT NOT NULL,
-	[Quanity] INT NOT NULL DEFAULT 1,
-	[PurchasePrice] MONEY NOT NULL,
-	[PurchaseDate] DATETIME2 NOT NULL DEFAULT getutcdate()
-)
