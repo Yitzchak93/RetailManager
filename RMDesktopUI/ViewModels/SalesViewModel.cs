8d6f92067db0f48190dd40140eca48d3cc40db89
Yitzchak Horowitz <yitzchakhorowitz321@gmail.com>
2021-01-02 16:10:24 +0200

    Get Add To Cart Working
    Adds the functionality to add items to the cart, get the subtotal, and update quantities.

--- RMDesktopUI/ViewModels/SalesViewModel.cs
+++ RMDesktopUI/ViewModels/SalesViewModel.cs
@@ -13,20 +13,24 @@ namespace RMDesktopUI.ViewModels
     public class SalesViewModel : Screen
     {
         private IProductEndpoint _productEndpoint;
+
         public SalesViewModel(IProductEndpoint productEndpoint)
         {
             _productEndpoint = productEndpoint;
         }
+
         protected override async void OnViewLoaded(object view)
         {
             base.OnViewLoaded(view);
             await LoadProducts();
         }
+
         private async Task LoadProducts()
         {
             var productList = await _productEndpoint.GetAll();
             Products = new BindingList<ProductModel>(productList);
         }
+
         private BindingList<ProductModel> _products;
 
         public BindingList<ProductModel> Products
@@ -39,9 +43,22 @@ namespace RMDesktopUI.ViewModels
             }
         }
 
-        private BindingList<ProductModel> _cart;
+        private ProductModel _selectedProduct;
 
-        public BindingList<ProductModel> Cart
+        public ProductModel SelectedProduct
+        {
+            get { return _selectedProduct; }
+            set
+            {
+                _selectedProduct = value;
+                NotifyOfPropertyChange(() => SelectedProduct);
+                NotifyOfPropertyChange(() => CanAddToCart);
+            }
+        }
+
+        private BindingList<CartItemModel> _cart = new BindingList<CartItemModel>();
+
+        public BindingList<CartItemModel> Cart
         {
             get { return _cart; }
             set
@@ -51,8 +68,7 @@ namespace RMDesktopUI.ViewModels
             }
         }
 
-
-        private int _itemQuantity;
+        private int _itemQuantity = 1;
 
         public int ItemQuantity
         {
@@ -61,36 +77,42 @@ namespace RMDesktopUI.ViewModels
             {
                 _itemQuantity = value;
                 NotifyOfPropertyChange(() => ItemQuantity);
+                NotifyOfPropertyChange(() => CanAddToCart);
             }
         }
 
-        public string SubTotoal
+        public string SubTotal
         {
             get
             {
+                decimal subTotal = 0;
+                foreach (var item in Cart)
+                {
+                    subTotal += (item.Product.RetailPrice * item.QuantityInCart);
+                }
                 // TODO - Replace with calculation
-                return "$0.00";
-
+                return subTotal.ToString("C");
             }
         }
+
         public string Tax
         {
             get
             {
                 // TODO - Replace with calculation
                 return "$0.00";
-
             }
         }
+
         public string Totoal
         {
             get
             {
                 // TODO - Replace with calculation
                 return "$0.00";
-
             }
         }
+
         public bool CanAddToCart
         {
             get
@@ -99,14 +121,37 @@ namespace RMDesktopUI.ViewModels
 
                 // Make sure something is selected
                 // Make sure there is an item quantity
+                if (ItemQuantity > 0 && SelectedProduct?.QuantityInStock >= ItemQuantity)
+                {
+                    output = true;
+                }
 
                 return output;
             }
-
         }
+
         public void AddToCart()
         {
-
+            CartItemModel existingItem = Cart.FirstOrDefault(x => x.Product == SelectedProduct);
+            if (existingItem != null)
+            {
+                existingItem.QuantityInCart += ItemQuantity;
+                // Hack - There should be a better way of refreshing the cart display.
+                Cart.Remove(existingItem);
+                Cart.Add(existingItem);
+            }
+            else
+            {
+                CartItemModel item = new CartItemModel
+                {
+                    Product = SelectedProduct,
+                    QuantityInCart = ItemQuantity
+                };
+                Cart.Add(item);
+            }
+            SelectedProduct.QuantityInStock -= ItemQuantity;
+            ItemQuantity = 1;
+            NotifyOfPropertyChange(() => SubTotal);
         }
 
         public bool CanRemoveFromCart
@@ -119,12 +164,13 @@ namespace RMDesktopUI.ViewModels
 
                 return output;
             }
-
         }
+
         public void RemoveFromCart()
         {
-
+            NotifyOfPropertyChange(() => SubTotal);
         }
+
         public bool CanCheckOut
         {
             get
@@ -135,13 +181,10 @@ namespace RMDesktopUI.ViewModels
 
                 return output;
             }
-
         }
+
         public void CheckOut()
         {
-
         }
-
-
     }
 }
