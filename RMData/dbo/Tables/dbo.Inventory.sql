86c7867be18e16acf1342c94af59a3fa791ac1fb
Yitzchak Horowitz <yitzchakhorowitz321@gmail.com>
2021-01-03 18:21:03 +0200

    Add Foreign key Relationships
    Adds FK relationships on tables.

---
+++ RMData/dbo/Tables/dbo.Inventory.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Inventory] (
+    --[Id]            INT           IDENTITY (1, 1) NOT NULL,
+    [Id]            INT     NOT NULL PRIMARY KEY      IDENTITY,
+	[ProductId]     INT           NOT NULL,
+    [Quanity]       INT           DEFAULT ((1)) NOT NULL,
+    [PurchasePrice] MONEY         NOT NULL,
+    [PurchaseDate]  DATETIME2 (7) DEFAULT (getutcdate()) NOT NULL, 
+    CONSTRAINT [FK_Inventory_ToProdcut] FOREIGN KEY ([ProductID]) REFERENCES Product(Id)
+    --PRIMARY KEY CLUSTERED ([Id] ASC) 
+);
+
