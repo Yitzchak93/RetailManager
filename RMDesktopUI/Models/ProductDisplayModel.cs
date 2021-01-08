0b5ae3a581eeae9752ee73660a60a29b81329553
Yitzchak Horowitz <yitzchakhorowitz321@gmail.com>
2021-01-08 15:53:59 +0200

    Update cart quantities
    Added Automapper and INPC to enable
    cart quantity changes to be reflected.

---
+++ RMDesktopUI/Models/ProductDisplayModel.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel;
+
+namespace RMDesktopUI.Models
+{
+    public class ProductDisplayModel : INotifyPropertyChanged
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public string Description { get; set; }
+        public decimal RetailPrice { get; set; }
+        private int _quantityInStoack;
+
+        public int QuantityInStock
+        {
+            get { return _quantityInStoack; }
+            set
+            {
+                _quantityInStoack = value;
+                CallPropertyChanged(nameof(QuantityInStock));
+            }
+        }
+
+        public bool IsTaxable { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void CallPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
