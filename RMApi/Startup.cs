fcb510935b23b5af3f5b5c0ea74557dc5bd87f95
Yitzchak Horowitz <yitzchakhorowitz321@gmail.com>
2021-01-24 14:38:29 +0200

    Moved security key to configuration

--- RMApi/Startup.cs
+++ RMApi/Startup.cs
@@ -54,7 +54,7 @@ namespace RMApi
                     {
 
                         ValidateIssuerSigningKey = true,
-                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("MySecretKeyIsSecretSoDoNotTell")),
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration.GetValue<string>("Secrets:SecurityKey"))),
                         ValidateIssuer = false,
                         ValidateAudience = false,
                         ValidateLifetime = true,
